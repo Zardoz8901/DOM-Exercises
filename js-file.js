b3a329d4d8a4032de8ed25440fc052ef36fac4dd
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-04-25 10:53:17 +0200

    Complete first DOM exercises

--- js-file.js
+++ js-file.js
@@ -5,3 +5,29 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const p = document.createElement('p');
+p.style.color = 'red';
+p.textContent = 'Hey I\'m red!';
+container.appendChild(p);
+
+const h3 = document.createElement('h3');
+h3.style.color = 'blue';
+h3.textContent = 'I\'m a blue h3!';
+container.appendChild(h3);
+
+const bdiv = document.createElement('div');
+bdiv.style.border = '5px';
+bdiv.style.borderStyle = 'solid';
+bdiv.style.borderColor = 'black';
+bdiv.style.backgroundColor = 'pink';
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I\'m in a div';
+bdiv.appendChild(h1);
+
+const pbdiv = document.createElement('p');
+pbdiv.textContent = 'ME TOO!';
+bdiv.appendChild(pbdiv);
+
+container.appendChild(bdiv);
