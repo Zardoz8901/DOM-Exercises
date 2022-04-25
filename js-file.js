7e03be9ab8c0cd31ab24309f31ab4532e7e0a896
Zardoz8901 <interzoneclearinghouse@gmail.com>
2022-04-25 14:51:44 +0200

    Complete exercise

--- js-file.js
+++ js-file.js
@@ -31,3 +31,21 @@ pbdiv.textContent = 'ME TOO!';
 bdiv.appendChild(pbdiv);
 
 container.appendChild(bdiv);
+
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+  }
+
+btn.addEventListener('click', alertFunction);
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
